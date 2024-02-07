bfe6f1c2c2c13fc1693258b4bf0b4730e840266b
James Tennant <jorahty@gmail.com>
2024-02-07 12:47:00 -0800

    list lectures 1-9

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+const descriptions = [
+  `logistics/administrivia/expectations; distributed systems: what and why?`,
+  `time and clocks; Lamport diagrams; causality and happens-before; network models; state and events`,
+  `happens-before recap; partial orders; total orders; Lamport clocks; vector clocks`,
+  `Lamport clocks recap; vector clocks (continued); delivery vs. receiving; some safety properties of executions: FIFO delivery, causal delivery; implementing FIFO delivery`,
+  `recap of FIFO/causal delivery; totally-ordered delivery; unicast/broadcast/multicast; implementing causal broadcast; introduction to distributed snapshots`,
+  `Chandy-Lamport snapshot algorithm`,
+  `Chandy-Lamport wrap-up; centralized vs. decentralized algorithms; safety and liveness; reliable delivery`,
+  `fault classification and fault models; two generals problem; common knowledge; forms of fault tolerance; idempotence and “exactly-once” delivery`,
+  `reliable broadcast; implementing reliable broadcast; reasons to do replication; strong consistency (informally); midterm review`,
+];
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+    <main>
+      {descriptions.map((v, i) => (
+        <a key={i} href={`Lecture-${i + 1}.pdf`}>
+          <h1>{i + 1}</h1>
+          <p>{v}</p>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
     </main>
   );
 }
